3253f9d895a69e541a683659ed17412ec95ddd70
jayeeed <jayedbinjahangir@gmail.com>
2024-03-17 05:37:48 +0600

    ud

--- app.py
+++ app.py
@@ -1,48 +1,85 @@
-from langchain_openai import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
-from langchain.schema import StrOutputParser
-from langchain.schema.runnable import Runnable
-from langchain.schema.runnable.config import RunnableConfig
+from operator import itemgetter
 
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.schema.output_parser import StrOutputParser
+from langchain.schema.runnable import Runnable, RunnablePassthrough, RunnableLambda
+from langchain.schema.runnable.config import RunnableConfig
+from langchain.memory import ConversationBufferMemory
+
+from chainlit.types import ThreadDict
 import chainlit as cl
 from typing import Dict, Optional
 
 
-@cl.on_chat_start
-async def on_chat_start():
-    model = ChatOpenAI(temperature=0.1, max_tokens=1024, streaming=True)
+def setup_runnable():
+    memory = cl.user_session.get("memory")
+    model = ChatOpenAI(streaming=True)
     prompt = ChatPromptTemplate.from_messages(
         [
-            (
-                "system",
-                "You're a helpful assistant. Answer as concisely as possible.",
-            ),
+            ("system", "You are a helpful chatbot"),
+            MessagesPlaceholder(variable_name="history"),
             ("human", "{question}"),
         ]
     )
-    runnable = prompt | model | StrOutputParser()
+
+    runnable = (
+        RunnablePassthrough.assign(
+            history=RunnableLambda(memory.load_memory_variables) | itemgetter("history")
+        )
+        | prompt
+        | model
+        | StrOutputParser()
+    )
     cl.user_session.set("runnable", runnable)
+
+
+@cl.oauth_callback
+def oauth_callback(
+    provider_id: str,
+    token: str,
+    raw_user_data: Dict[str, str],
+    default_user: cl.User,
+) -> Optional[cl.User]:
+    return default_user
+
+
+@cl.on_chat_start
+async def on_chat_start():
+    cl.user_session.set("memory", ConversationBufferMemory(return_messages=True))
+    setup_runnable()
+
+
+@cl.on_chat_resume
+async def on_chat_resume(thread: ThreadDict):
+    memory = ConversationBufferMemory(return_messages=True)
+    root_messages = [m for m in thread["steps"] if m["parentId"] == None]
+    for message in root_messages:
+        if message["type"] == "USER_MESSAGE":
+            memory.chat_memory.add_user_message(message["output"])
+        else:
+            memory.chat_memory.add_ai_message(message["output"])
+
+    cl.user_session.set("memory", memory)
+
+    setup_runnable()
 
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    runnable = cl.user_session.get("runnable")  # type: Runnable
+    memory = cl.user_session.get("memory")
 
-    msg = cl.Message(content="")
+    runnable = cl.user_session.get("runnable")
+
+    res = cl.Message(content="")
 
     async for chunk in runnable.astream(
         {"question": message.content},
         config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
     ):
-        await msg.stream_token(chunk)
+        await res.stream_token(chunk)
 
-    await msg.send()
+    await res.send()
 
-@cl.oauth_callback
-def oauth_callback(
-  provider_id: str,
-  token: str,
-  raw_user_data: Dict[str, str],
-  default_user: cl.User,
-) -> Optional[cl.User]:
-  return default_user
+    memory.chat_memory.add_user_message(message.content)
+    memory.chat_memory.add_ai_message(res.content)
