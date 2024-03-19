aff1bf678190219eccc63b5d0f252914d89197be
jayeeed <jayedbinjahangir@gmail.com>
2024-03-20 05:11:53 +0900

    llama2

--- llama2.py
+++ llama2.py
@@ -1,47 +1,43 @@
 from langchain.llms.huggingface_pipeline import HuggingFacePipeline
 from transformers import AutoTokenizer, TextStreamer
-from langchain.prompts import PromptTemplate
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.memory import ConversationBufferMemory
 from langchain.chains import LLMChain
 import transformers
 import torch
 
+from operator import itemgetter
+from chainlit.types import ThreadDict
 import chainlit as cl
+from typing import Dict, Optional
 from chainlit.playground.config import add_llm_provider
 from chainlit.playground.providers.langchain import LangchainGenericProvider
-
-
-template = """
-You are a helpful AI assistant. Provide the answer for the following question:
-
-Question: {question}
-Answer:
-"""
+from langchain.schema.output_parser import StrOutputParser
+from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
+from langchain.schema.runnable.config import RunnableConfig
 
 
 @cl.cache
 def load_llama():
     model = "meta-llama/Llama-2-7b-chat-hf"
     tokenizer = AutoTokenizer.from_pretrained(model)
-    streamer = TextStreamer(tokenizer, skip_prompt=True)
+    streamer = TextStreamer(tokenizer)
     pipeline = transformers.pipeline(
-        "text-generation",
+        task="text-generation",
         model=model,
         tokenizer=tokenizer,
         torch_dtype=torch.float16,
         trust_remote_code=True,
-        device_map="auto",
-        max_length=1000,
-        do_sample=True,
-        top_k=10,
-        num_return_sequences=1,
-        eos_token_id=tokenizer.eos_token_id,
+        return_full_text=True,
+        temperature=0.5,
+        max_new_tokens=256,
+        repetition_penalty=1.5,
         streamer=streamer,
-        use_auth_token=True,
     )
 
     llm = HuggingFacePipeline(
         pipeline=pipeline,
-        model_kwargs={"temperature": 0, "max_length": 1000, "streaming": True, "pad_token_id": 0, "eos_token_id": 0, "device_map": "cuda", "use_auth_token": True},
+        model_kwargs={"temperature": 0}, 
     )
     return llm
 
@@ -50,29 +46,78 @@ llm = load_llama()
 
 add_llm_provider(
     LangchainGenericProvider(
-        id=llm._llm_type, name="Llama2-chat", llm=llm, is_chat=False
+        id=llm._llm_type, name="Llama2-chat", llm=llm, is_chat=True
     )
 )
 
+def setup_runnable():
+    memory = cl.user_session.get("memory")
+    model = llm
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are a python coding assistant that can write python code."),
+            MessagesPlaceholder(variable_name="history"),
+            ("user", "{question}"),
+            ("ai", " "),
+        ]
+    )
+
+    runnable = (
+        RunnablePassthrough.assign(
+            history=RunnableLambda(memory.load_memory_variables) | itemgetter("history")
+        )
+        | prompt
+        | model
+        | StrOutputParser()
+    )
+    cl.user_session.set("runnable", runnable)
+
+
+@cl.oauth_callback
+def oauth_callback(
+    provider_id: str,
+    token: str,
+    raw_user_data: Dict[str, str],
+    default_user: cl.User,
+) -> Optional[cl.User]:
+    return default_user
+
 
 @cl.on_chat_start
-async def main():
-    prompt = PromptTemplate(template=template, input_variables=["question"])
-    llm_chain = LLMChain(prompt=prompt, llm=llm)
-    cl.user_session.set("llm_chain", llm_chain)
+async def on_chat_start():
+    cl.user_session.set("memory", ConversationBufferMemory(return_messages=True))
+    setup_runnable()
 
-    return llm_chain
+@cl.on_chat_resume
+async def on_chat_resume(thread: ThreadDict):
+    memory = ConversationBufferMemory(return_messages=True)
+    root_messages = [m for m in thread["steps"] if m["parentId"] == None]
+    for message in root_messages:
+        if message["type"] == "USER_MESSAGE":
+            memory.chat_memory.add_user_message(message["output"])
+        else:
+            memory.chat_memory.add_ai_message(message["output"])
+
+    cl.user_session.set("memory", memory)
+
+    setup_runnable()
 
 
 @cl.on_message
-async def run(message: cl.Message):
-    cb = cl.AsyncLangchainCallbackHandler(
-        stream_final_answer=True, answer_prefix_tokens=["Answer"]
-    )
+async def on_message(message: cl.Message):
+    memory = cl.user_session.get("memory")
 
-    # Retrieve the chain from the user session
-    llm_chain = cl.user_session.get("llm_chain")  # type: LLMChain
-    res = await llm_chain.ainvoke(message.content, callbacks=[cb])
+    runnable = cl.user_session.get("runnable")
 
-    if not cb.answer_reached:
-        await cl.Message(content=res["text"]).send()
+    res = cl.Message(content="")
+
+    async for chunk in runnable.astream(
+        {"question": message.content},
+        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+    ):
+        await res.stream_token(chunk)
+
+    await res.send()
+
+    memory.chat_memory.add_user_message(message.content)
+    memory.chat_memory.add_ai_message(res.content)
